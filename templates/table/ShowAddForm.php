1d5b3d702143c9d60a45ad830a46a8c6fc5e5907
iconsofevill <47002949+iconsofevill@users.noreply.github.com>
2019-10-15 20:14:25 +0300

    prev and next buttons + new table controller

--- templates/table/ShowAddForm.php
+++ templates/table/ShowAddForm.php
@@ -1,9 +1,9 @@
 <div>
     <form action="<?= $AddURL ?>" method="POST" class="text-center border border-light p-5">
         <?php
-            foreach ($Add as $key => $value) {
-                if ($key != 0) {
-                    echo "<label><input class='form-control mb-4' type='text' name='".$value."' placeholder='".$value."'></label><br/>";
+            foreach ($ColumnsNames as $name) {
+                if ($name != 'id') {
+                    echo "<label><input class='form-control mb-4' type='text' name='".$name."' placeholder='".$name."'></label><br/>";
                 }
             }
         ?>
