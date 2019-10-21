8ef6ba43c6f8397a38f57fd8427c3a615d8bd40f
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-10-21 23:06:11 +0300

    update ShowForm for registration

--- templates/signup/showForm.php
+++ templates/signup/showForm.php
@@ -1,8 +1,21 @@
     <form action="<?= $SignUpURL ?>" method="POST" class="text-center border border-light p-5 ">
-        <input type="text" name="name" id="" required value="<?= $newSignUpDate['name'] ?? '' ?>">
-        <input type="text" name="surname" id="" required value="<?= $newSignUpDate['surname'] ?? '' ?>">
-        <input type="text" name="login" id="" required value="<?= $newSignUpDate['login'] ?? '' ?>">
-        <input type="password" name="pass" id="" required>
-        <input type="password" name="passrepeat" id="" required>
-        <input type="submit" value="Регистрация">
+        <div class="form-group">
+            <input type="text" name="name" id="" required value="<?= $newSignUpDate['name'] ?? '' ?>" placeholder = "Введите имя" class="form-control">
+        </div> 
+        <div class="form-group">    
+            <input type="text" name="surname" id="" required value="<?= $newSignUpDate['surname'] ?? '' ?>" placeholder = "Введите фамилию" class="form-control">
+        </div>
+        <div class="form-group">    
+            <input type="text" name="login" id="" required value="<?= $newSignUpDate['login'] ?? '' ?>" placeholder = "Введите логин" class="form-control">
+        </div>    
+        <div class="form-group">
+            <input type="password" name="pass" id="" required placeholder = "Введите пароль" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" name="passrepeat" id="" required placeholder = "Повторите пароль" class="form-control">         
+        </div>
+            <input type="submit" value="Регистрация" class="btn btn-primary">
     </form>
+    <div>
+        <?=$_SESSION['SignUpError']?>
+    </div>
