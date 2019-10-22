37481788683317f005fb15f218fb02d3d874685f
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-10-22 19:59:01 +0300

    add login

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Core\Auth;
 use App\Model\UsersModel;
 use TexLab\MyDB\DB;
 use App\Core\Conf;
@@ -14,29 +15,29 @@ class SignUpController extends AbstractController
 
     public function actionShowForm()
     {
+        $newSignUpDate = $_SESSION['checkSignUpData']['Data'] ?? [];
+        $signUpErrors = $_SESSION['checkSignUpData']['Errors'] ?? [];
 
+        unset($_SESSION['checkSignUpData']);
 
         $this->render("showForm", [
             // 'columnsNames' => $this->table->getColumnsNames(),
             // 'editValues' => $this->table->get(['id' => $_GET['id']])[0],
-            'SignUpURL' => '?t=' . $this->shortClassName() . '&a=checkNewUser',
-            'newSignUpDate' => $_SESSION['newSignUpDate'] ?? [],
-            'SignUpError' => $_SESSION['SignUpError'] ?? '',
+            'SignUpURL' => '?t=' . $this->shortClassName() . '&a=registerUser',
+            'newSignUpDate' => $newSignUpDate,
+            'signUpErrors' => $signUpErrors,
 
             // 'tableHeaders' => $this->table->getColumnsComments()
         ]);
     }
-    public function actionCheckNewUser()
+    public function actionRegisterUser()
     {
-        if ($_POST['pass'] != $_POST['passrepeat']) {
-            $_SESSION['newSignUpDate'] = $_POST;
-            $_SESSION['SignUpError'] = "пароли не совпадают";
+        if ($this->checkRegisterData($_POST)) {
+            $_SESSION['checkSignUpData']['Data'] = $_POST;
             $this->redirect('?t=' . $this->shortClassName() . '&a=showform');
         }
 
         $userGroupId = (new Table('user_group', DB::Link(Conf::MYSQL)))->get(['cod' => 'dft'])[0]['id'];
-
-
 
         (new Table('users', DB::Link(Conf::MYSQL)))->add([
             "name" => $_POST["name"],
@@ -45,6 +46,34 @@ class SignUpController extends AbstractController
             "pass" => $_POST["pass"],
             "user_group_id" => $userGroupId
         ]);
-        $this->redirect('?a=loginform');
+
+        Auth::registerUser($_POST["login"], $_POST["pass"]);
+
+        $this->redirect('?a=home');
+    }
+
+    private function checkRegisterData($data)
+    {
+        if ($data['pass'] != $data['passrepeat']) {
+            $_SESSION['checkSignUpData']['Errors'][] = "Пароли не совпадают";
+        }
+
+        if (empty($data['pass'])) {
+            $_SESSION['checkSignUpData']['Errors'][] = "Введите пароль";
+        }
+
+        if (empty($data['login'])) {
+            $_SESSION['checkSignUpData']['Errors'][] = "Введите логин";
+        }
+
+        if (!empty((new Table('users', DB::Link(Conf::MYSQL)))->get(['login'=> $data['login']]))) {
+            $_SESSION['checkSignUpData']['Errors'][] = "Этот логин занят!";
+        }
+
+        if (empty($_SESSION['checkSignUpData'])) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- templates/signup/showForm.php
+++ templates/signup/showForm.php
@@ -1,21 +1,26 @@
+    <div>
+        <?php
+            foreach ($signUpErrors as $key => $value) {
+                echo "<div class='alert alert-danger' role='alert'>$value</div>";
+            } 
+        ?>
+    </div>
     <form action="<?= $SignUpURL ?>" method="POST" class="text-center border border-light p-5 ">
         <div class="form-group">
-            <input type="text" name="name" id="" required value="<?= $newSignUpDate['name'] ?? '' ?>" placeholder = "Введите имя" class="form-control">
+            <input type="text" name="name" value="<?= $newSignUpDate['name'] ?? '' ?>" placeholder = "Введите имя" class="form-control">
         </div> 
         <div class="form-group">    
-            <input type="text" name="surname" id="" required value="<?= $newSignUpDate['surname'] ?? '' ?>" placeholder = "Введите фамилию" class="form-control">
+            <input type="text" name="surname" value="<?= $newSignUpDate['surname'] ?? '' ?>" placeholder = "Введите фамилию" class="form-control">
         </div>
         <div class="form-group">    
-            <input type="text" name="login" id="" required value="<?= $newSignUpDate['login'] ?? '' ?>" placeholder = "Введите логин" class="form-control">
+            <input type="text" name="login" value="<?= $newSignUpDate['login'] ?? '' ?>" placeholder = "Введите логин" class="form-control">
         </div>    
         <div class="form-group">
-            <input type="password" name="pass" id="" required placeholder = "Введите пароль" class="form-control">
+            <input type="password" name="pass" placeholder = "Введите пароль" class="form-control">
         </div>
         <div class="form-group">
-            <input type="password" name="passrepeat" id="" required placeholder = "Повторите пароль" class="form-control">         
+            <input type="password" name="passrepeat" placeholder = "Повторите пароль" class="form-control">         
         </div>
             <input type="submit" value="Регистрация" class="btn btn-primary">
     </form>
-    <div>
-        <?=$_SESSION['SignUpError']?>
-    </div>
+    
